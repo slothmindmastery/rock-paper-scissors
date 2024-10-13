2d1f8e12e4cc2ea050529f9ce9ab8565d9e668ac
slothmindmastery <100491009+slothmindmastery@users.noreply.github.com>
2024-10-13 22:47:04 +0100

    Add buttons and logic
    - creeate js function to add buttons
    - create js function to listen for click event
    - button click event triggers playRound function

--- javascript.js
+++ javascript.js
@@ -1,31 +1,11 @@
-// Create a new function called getHumanChoice
-function getHumanChoice() {
-  // prompt user to enter choice
-  humanChoice = prompt("rock, paper or scissors?")
-  // make text case insensitive
-  humanChoice = humanChoice.toLowerCase();
-  // return one of the valid choices depending on what the user inputs
-  switch(humanChoice) {
-    case "rock":
-      break;
-    case "paper":
-      break;
-    case "scissors":
-      break;
-    default:
-      console.log("not a valid input");
-      getHumanChoice();
-  }
-  console.log("You have chosen " + humanChoice);
-  return humanChoice;
-}
+const options = ["rock", "paper", "scissors"];
 
-// Create a new function called getComputerChoice
-function getComputerChoice() {
-  // generate random number between one and three
-  let choiceNumber = Math.floor(Math.random() * 3)
-  // use conditional statement to match each choice to either "rock", "paper" or "scissors" and return value
-  let computerChoice = "error";
+let humanScore = 0;
+let computerScore = 0;
+
+const getComputerChoice = function() {
+  const choiceNumber = Math.floor(Math.random() * 3)
+  let computerChoice = "";
   switch(choiceNumber) {
     case 0:
       computerChoice = "rock";
@@ -36,107 +16,86 @@ function getComputerChoice() {
     case 2:
       computerChoice = "scissors";
       break;
-    default:
-      computerChoice = "error - the computer has made no choice"
   }
   console.log("The computer has chosen " + computerChoice);
   return computerChoice;
 }
 
-// Create a new funtion named playRound with parameters to take the human and computer choices as arguments
-
-// Create a function called playGame
-function playGame(){
-  // Declare playRound function and score variables so that they are declared inside playGame function
-  function playRound(humanPlay, computerPlay) {
-    let decision = "error";
-    if (humanPlay == "rock") {
-      switch (computerPlay){
-        case "paper":
-          decision = "You lose!";
-          computerScore ++;
-          break;
-        case "scissors":
-          decision = "You win!";
-          humanScore ++;
-          break;
-        default:
-          decision = "It's a draw!";
-      }
+const playRound = function(humanChoice) {
+  const computerChoice = getComputerChoice();
+  let decision = "";
+  if (humanChoice == options[0]) {
+    switch (computerChoice){
+      case options[1]:
+        decision = "lose";
+        computerScore ++;
+        break;
+      case "scissors":
+        decision = "You win!";
+        humanScore ++;
+        break;
+      default:
+        decision = "It's a draw!";
     }
-    else if (humanPlay == "paper") {
-      switch (computerPlay){
-        case "scissors":
-          decision = "You lose!";
-          computerScore ++;
-          break;
-        case "rock":
-          decision = "You win!";
-          humanScore ++;
-          break;
-        default:
-          decision = "It's a draw!";
-      }
+  }
+  else if (humanChoice == "paper") {
+    switch (computerChoice){
+      case "scissors":
+        decision = "You lose!";
+        computerScore ++;
+        break;
+      case "rock":
+        decision = "You win!";
+        humanScore ++;
+        break;
+      default:
+        decision = "It's a draw!";
     }
-    else if (humanPlay == "scissors") {
-      switch (computerPlay){
-        case "rock":
-          decision = "You lose!";
-          computerScore ++;
-          break;
-        case "paper":
-          decision = "You win!";
-          humanScore ++;
-          break;
-        default:
-          decision = "It's a draw!";
-      }
+  }
+  else if (humanChoice == "scissors") {
+    switch (computerChoice){
+      case "rock":
+        decision = "You lose!";
+        computerScore ++;
+        break;
+      case "paper":
+        decision = "You win!";
+        humanScore ++;
+        break;
+      default:
+        decision = "It's a draw!";
     }
-    return decision;
   }
-  let humanScore = 0;
-  let computerScore = 0;
-
-  // Play 5 rounds by calling playRound 5 times
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
-  console.log(playRound(humanSelection, computerSelection));
-
-  console.log("Human score is " + humanScore + " and Computer score is " + computerScore);
-  
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  console.log(playRound(humanSelection, computerSelection));
-
-  console.log("Human score is " + humanScore + " and Computer score is " + computerScore);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  console.log(playRound(humanSelection, computerSelection));
-
-  console.log("Human score is " + humanScore + " and Computer score is " + computerScore);
-  
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  console.log(playRound(humanSelection, computerSelection));
-
-  console.log("Human score is " + humanScore + " and Computer score is " + computerScore);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-  console.log(playRound(humanSelection, computerSelection));
-
-  console.log("Human score is " + humanScore + " and Computer score is " + computerScore);
-
-  if (humanScore > computerScore) {
-    console.log("Human is the overall winner! Congratulations!!!")
-  }
-  else if (humanScore < computerScore) {
-    console.log("Human is the overall Loser! Better luck next time!")
-  }
-  else {
-    console.log("It's a draw! Try harder next time and you might win!")
-  }
+  console.log(decision);
+  return decision;
 }
 
-playGame ();
+const buttonsSetup = function(){
+  const optionButtons = document.createElement("div");
+  optionButtons.setAttribute("id", "optionButtons")
+  document.body.appendChild(optionButtons);
+  const rockButton = document.createElement("button");
+  rockButton.textContent = "rock";
+  optionButtons.appendChild(rockButton);
+  const paperButton = document.createElement("button");
+  paperButton.textContent = "paper";
+  optionButtons.appendChild(paperButton);
+  const scissorsButton = document.createElement("button");
+  scissorsButton.textContent = "scissors";
+  optionButtons.appendChild(scissorsButton);
+}
+
+const buttonClick = function() {
+  optionButtons = document.querySelector("#optionButtons");
+  optionButtons.addEventListener('click', event => {
+    if (event.target.matches("button")){
+      humanChoice = event.target.textContent;
+      console.log("You have chosen " + humanChoice + " !")
+      playRound(humanChoice);
+      return;
+    }
+  })
+}
+
+buttonsSetup();
+buttonClick();
